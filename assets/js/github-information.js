cd9628e8b939d23404beba50bd093986df75167a
ErikaN-ir <erika.nethaway@gmail.com>
2020-03-30 11:29:08 +0000

    Another attempt at fixing error message

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -2,7 +2,7 @@ function userInformationHTML(user) {
     return `
     <h2>${user.name}
         <span class="small-name">
-        (@<a href="${user.html_url}" target="_blank"></a>
+        (@<a href="${user.html_url}" target="_blank">${user.login}</a>
         </span>
     </h2>
     <div class="gh-content">
@@ -72,7 +72,7 @@ function fetchGitHubInformation(event) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
             } else if (errorResponse.status === 403) {
-                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
                 $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
